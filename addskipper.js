ac5f523955e24badbe4dc87c0b8df61cad28ea62
Victor T <victor.tim@icloud.com>
2017-03-01 09:59:51 -0500

    implement activate button

--- addskipper.js
+++ addskipper.js
@@ -2,13 +2,13 @@
 const $ = el => document.querySelector(el)
 const skipAdd = () => $('video').currentTime = $('video').duration
 
+console.log('running')
 
 let btn = document.createElement('button')
     btn.textContent = 'Skip Add'
     btn.className = 'addskipper'
-    btn.style.cssText = 'margin: 0 auto;width: 50px; position:fixed;top: 10%;left: 1%; background-color: #fff; z-index: 999999; border-radius: 15px; cursor: pointer'
+    btn.style.cssText =
+    'margin: 0 auto;width: 50px;position:fixed;top: 10%;left: 1%;background-color: #fff;z-index: 999999;border-radius: 15px;cursor: pointer'
     btn.addEventListener('click', skipAdd)
+
 document.body.appendChild(btn)
-
-
-console.log('running')
