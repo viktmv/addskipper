7e8cf80a436e156ef3a5b55b16537cdc07e4bc2d
Victor T <victor.tim@icloud.com>
2017-03-01 19:27:12 -0500

    reorganize app structure

--- skip.js
+++ skip.js
@@ -1,11 +1,10 @@
 
+console.log('skipper running')
 const $ = el => document.querySelector(el)
 const skipAdd = () => $('video').currentTime = $('video').duration
 
-console.log('running')
-
 let btn = document.createElement('button')
-    btn.textContent = 'Skip Add'
+    btn.textContent = 'Skip'
     btn.className = 'addskipper'
     btn.style.cssText =
     'margin: 0 auto;width: 50px;position:fixed;top: 10%;left: 1%;background-color: #fff;z-index: 999999;border-radius: 15px;cursor: pointer'
